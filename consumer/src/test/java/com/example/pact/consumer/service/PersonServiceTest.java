e62bab5d8ae365153d1657084517d31486383836
Michael Simon <5222293+micsimon@users.noreply.github.com>
2019-12-08 15:43:55 +0100

    fixed pact verification

--- consumer/src/test/java/com/example/pact/consumer/service/PersonServiceTest.java
+++ consumer/src/test/java/com/example/pact/consumer/service/PersonServiceTest.java
@@ -1,7 +1,9 @@
 package com.example.pact.consumer.service;
 
+import au.com.dius.pact.consumer.dsl.PactDslJsonBody;
 import au.com.dius.pact.consumer.dsl.PactDslWithProvider;
 import au.com.dius.pact.consumer.junit.PactProviderRule;
+import au.com.dius.pact.consumer.junit.PactVerification;
 import au.com.dius.pact.core.model.RequestResponsePact;
 import au.com.dius.pact.core.model.annotations.Pact;
 import com.example.pact.consumer.model.Person;
@@ -10,36 +12,40 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class PersonServiceTest {
 
     private PersonService underTest;
 
     @Rule
-    public PactProviderRule mockProvider = new PactProviderRule("Provider", "localhost", 8080, this);
+    public PactProviderRule mockProvider = new PactProviderRule("ProviderName", null, 8080, this);
 
     @Before
     public void setup() {
         underTest = new PersonService();
     }
 
-    @Pact(consumer = "JUnitRuleConsumer")
+    @Pact(consumer = "ConsumerName")
     public RequestResponsePact createPact(PactDslWithProvider builder) {
-        return builder.given("")
-                      .uponReceiving("Pact JVM example")
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Content-Type", "application/json;charset=UTF-8");
+
+        return builder.uponReceiving("get person from provider")
                       .path("/person")
                       .method("GET")
                       .willRespondWith()
                       .status(200)
-                      .body("{" +
-                              "\name\" : \"Hans\"" +
-                              "\"age\"+:\"113\"" +
-                              "}")
+                      .headers(headers)
+                      .body(new PactDslJsonBody().and("name", "Hans")
+                                                 .and("age", 11))
                       .toPact();
     }
 
+    @PactVerification
     @Test
     public void firstPactTest() {
-
         Person actual = underTest.getPersonFromProvider();
 
         Assert.assertEquals("Hans", actual.getName());
