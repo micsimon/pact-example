82c31bbd99b4df935d2352b38a7bf54e00b8f15d
Michael Simon <5222293+micsimon@users.noreply.github.com>
2019-12-09 06:54:59 +0100

    added not correct working pact provider-test ... need to fix

--- provider/src/main/java/com/example/pact/provider/controller/PersonController.java
+++ provider/src/main/java/com/example/pact/provider/controller/PersonController.java
@@ -1,13 +1,14 @@
 package com.example.pact.provider.controller;
 
 import com.example.pact.provider.model.Person;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PersonController {
 
-    @GetMapping("/person")
+    @GetMapping(value = "/person", produces = {MediaType.APPLICATION_JSON_UTF8_VALUE})
     public Person getPerson() {
         return Person.builder()
                      .age(11)
