80c18a46e380a44d5629152723c6c957831352f3
Michael Simon <5222293+micsimon@users.noreply.github.com>
2019-12-29 17:20:43 +0100

    fixed pact test execution

--- provider/src/test/java/com/example/pact/provider/controller/PersonControllerTest.java
+++ provider/src/test/java/com/example/pact/provider/controller/PersonControllerTest.java
@@ -1,48 +1,21 @@
 package com.example.pact.provider.controller;
 
 import au.com.dius.pact.provider.junit.Provider;
-import au.com.dius.pact.provider.junit.State;
 import au.com.dius.pact.provider.junit.loader.PactBroker;
 import au.com.dius.pact.provider.junit.target.HttpTarget;
 import au.com.dius.pact.provider.junit.target.Target;
 import au.com.dius.pact.provider.junit.target.TestTarget;
 import au.com.dius.pact.provider.spring.SpringRestPactRunner;
 import com.example.pact.provider.ProviderApplication;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.web.context.ConfigurableWebApplicationContext;
 
 @RunWith(SpringRestPactRunner.class)
 @Provider("ProviderName")
 @PactBroker(host = "localhost")
-@SpringBootTest
+@SpringBootTest(classes = ProviderApplication.class, webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class PersonControllerTest {
 
-    @InjectMocks
-    private PersonController personController = new PersonController();
-
-//    @TestTarget
-//    public MockMvcTarget mockMvcTarget = new MockMvcTarget();
-
     @TestTarget
-    public final Target target = new HttpTarget("http", "localhost", 8080, "/");
-
-    private static ConfigurableWebApplicationContext application;
-
-    @BeforeAll
-    public static void setup() {
-        application = (ConfigurableWebApplicationContext)
-                SpringApplication.run(ProviderApplication.class);
-//        mockMvcTarget.setControllers(personController);
-    }
-
-    @Test
-    @State("getPerson")
-    public void asd() {
-    }
-
+    public final Target target = new HttpTarget(8080);
 }
